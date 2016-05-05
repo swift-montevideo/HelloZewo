42e7c3d023a8bfd110beece8087eb435783f392b
Bruno Berisso <bruno@tryolabs.com>
2016-05-05 02:00:52 -0300

    First successful data connection from PostgreSQL

--- Sources/Todo.swift
+++ Sources/Todo.swift
@@ -1,21 +1,64 @@
 import Mustache
+import PostgreSQL
 
 
 struct TodoApp {
 
-    func todoList() throws -> String {
-        // Load the `document.mustache` resource of the main bundle
-        let templateDef = "Hello {{name}}\n{{#late}}Well, on die{{/late}}"
-        let template = try Template(string: templateDef)
+    struct Todo {
+        let id: Int
+        let text: String
 
-        // The rendered data
-        let data = [
-            "name": "Arthur".mustacheBox,
-            "late": false.mustacheBox
-        ]
+        init(id _id: Int, text _text: String) {
+            id = _id
+            text = _text
+        }
+    }
 
-        // The rendering: "Hello Arthur..."
-        return try template.render(box: data.mustacheBox)
+    private func getAllTodos() throws -> [Todo] {
+        let connection = Connection(host: "192.168.99.100", databaseName: "postgres", username: "postgres")
+        try connection.open()
+
+        // for row in try connection.execute("SELECT * FROM TODOS") {
+        //     let id: Int = try row.value("id")
+        //     print(id)
+        // }
+
+        return try connection.execute("SELECT * FROM TODOS")
+            .map { row in
+                let id: Int = try row.value("id")
+                let text: String = try row.value("text")
+                return Todo(id: id, text: text)
+            }
+
+        // return []
+    }
+
+    func todoList() -> String {
+        do {
+            let todos = try getAllTodos()
+            print(todos)
+        } catch {
+            return "Database Error"
+        }
+
+        do {
+            // Load the `document.mustache` resource of the main bundle
+            let templateDef = "Hello {{name}}\n{{#late}}Well, on die{{/late}}"
+            let template = try Template(string: templateDef)
+
+            // The rendered data
+            let data = [
+                "name": "Arthur".mustacheBox,
+                "late": false.mustacheBox
+            ]
+
+            // The rendering: "Hello Arthur..."
+            return try template.render(box: data.mustacheBox)
+        } catch let e as MustacheError {
+            return e.description
+        } catch {
+            return "Strange error"
+        }
     }
 
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -2,7 +2,37 @@ import HTTPServer
 import Router
 import LogMiddleware
 import StandardOutputAppender
-import Mustache
+// import PostgreSQL
+//
+// func test(a: Int) -> String? {
+//     return "1 \(a)"
+// }
+//
+// func test(_ a: Int) -> String {
+//     return (test(a: a) as String?)!
+// }
+//
+// if let a = test(a: 6) {
+//     print(a)
+// }
+//
+// print(test(3))
+// exit(0)
+//
+// let connection = Connection(host: "192.168.99.100", databaseName: "postgres", username: "postgres")
+// try connection.open()
+//
+// for row in try connection.execute("SELECT * FROM TODOS") {
+//     // if let id: Data = try row.data(field: "id") {
+//     //     print(id)
+//     // } else {
+//     //     print(row)
+//     // }
+//
+//     let id: Int = try row.value("id")
+//     print(id)
+//     print(row)
+// }
 
 let log = Logger(name: "example-logger", appender: StandardOutputAppender(), levels: .info)
 let logMidd = LogMiddleware(logger: log)
@@ -11,13 +41,7 @@ let app = TodoApp()
 
 let router = Router { route in
     route.get("/") { _ in
-
-        do {
-            let responseStr = try app.todoList()
-            return Response(body: responseStr)
-        } catch let e as MustacheError {
-            return Response(body: e.description)
-        }
+        return Response(body: app.todoList())
         // return Response(body: "<BODY><HEAD>hello world</HEAD></BODY>")
     }
 }
